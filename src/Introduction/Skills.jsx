2595b8920157f1547e202e8a8bd24e450f285867
ravinabisht2004 <ravinasinghbisht159677@gmail.com>
2026-07-15 11:50:02 +0530

    feat - adding skills files

--- src/Introduction/Skills.jsx
+++ src/Introduction/Skills.jsx
@@ -1,14 +1,54 @@
 import React from "react";
 import { ChevronsLeftRight, icons } from "lucide-react";
 import { IoLogoHtml5 } from "react-icons/io5";
+import { FaCss3 } from "react-icons/fa";
+import { FaJs } from "react-icons/fa6";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { DiNodejs } from "react-icons/di";
+import { SiExpress } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+
+
+
 
 export default function Skills() {
-    const Skills = [
-        {icon : <IoLogoHtml5 />,
-          title: 'HTML',  
-        },
+  const Skills = [
+    { icon: <IoLogoHtml5 color="orange" size={30}/>,
+      title: "HTML" 
+    },
+    {
+      icon: <FaCss3 color="Blue" size={30}/>,
+      title:"CSS"
+    },
+    {
+      icon: <FaJs  color="Yellow" size={30}/>,
+      title:"JavaScript"
+    },
+    {
+      icon: <FaReact  color="SkyBlue" size={30}/>,
+      title:"React"
+    },
+    {
+      icon: <RiTailwindCssFill  color="SkyBlue" size={30}/>,
+      title:"Tailwind CSS"
+    },
+    {
+      icon: <DiNodejs  color="green" size={30}/>,
+      title:"Node JS"
+    },
+    {
+      icon: <SiExpress   size={30}/>,
+      title:"Express.js"
+    },
+    {
+      icon: <GrMysql   size={30}/>,
+      title:"My Sql"
+    },
 
-    ]
+
+
+  ];
 
   return (
     <div className="p-3 border-2 border-purple-500 w-fit h-80 rounded-2xl">
@@ -17,7 +57,19 @@ export default function Skills() {
           <ChevronsLeftRight color="purple" size={30} />
         </div>
         <div>Skills</div>
-      </div>
+        </div>
+
+        <div className="grid grid-cols-3 gap-2">
+          {Skills.map((Key, index) => (
+            
+            <div className="flex items-center mt-3 gap-2 ">
+              <div>{Key.icon}</div>
+              <div>{Key.title}</div>
+            </div>
+            
+          ))}
+        </div>
+      
     </div>
   );
 }
